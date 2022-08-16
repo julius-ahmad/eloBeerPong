c2060474877115857d03ff4507ee36dde7877e6e
julius-ahmad <julius.ahmad02@gmail.com>
2022-08-16 15:45:26 +0100

    Implemented data handler in nicer format

---
+++ DataHandler.py
@@ -0,0 +1,12 @@
+def read_data():
+    """
+    reads data from the games.txt file
+    @return: a list of tuples of players who played in a game and who won (number represents if 2 left players won)
+    """
+    with open('games.txt') as f:
+        games = [game.split() for game in (line.strip() for line in f.readlines())]
+    f.close()
+
+    games = [(game[:4], game[4]) for game in games]
+
+    return games
