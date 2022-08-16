930948614bc0a78202cfae7ab5aa17cc44a8e85d
julius-ahmad <julius.ahmad02@gmail.com>
2022-08-16 15:55:04 +0100

    added some comments clarifying 2v2 setup

--- DataHandler.py
+++ DataHandler.py
@@ -1,6 +1,6 @@
 def read_data():
     """
-    reads data from the games.txt file
+    reads data from the games.txt file keeps players in order as we have 2v2 matches
     @return: a list of tuples of players who played in a game and who won (number represents if 2 left players won)
     """
     with open('games.txt') as f:
